5fe5c31fd1c9cdccb79d4ae96dde981b408f0e4a
Fabian Naumiuk <fabian.naumiuk@gmail.com>
2022-07-03 16:31:29 +0200

    update

---
+++ ALXCSharpCourse/Homework/M1/Kursy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALXCSharpCourse.Homework.M1
+{
+    class Kursy : Produkty
+    {
+        public override void pokazInfo()
+        {
+            Console.WriteLine("Kursy");
+        }
+    }
+}
---
+++ ALXCSharpCourse/Homework/M1/Produkty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ALXCSharpCourse.Homework.M1
+{
+    abstract class Produkty
+    {
+        abstract public void pokazInfo();
+        
+    }
+}
